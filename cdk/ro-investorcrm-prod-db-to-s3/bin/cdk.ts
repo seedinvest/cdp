c103703c496fd10a930a89f39bb18322d173bd5f
Nick Pate <nicholas.pate@seedinvest.com>
2022-03-30 13:17:13 -0700

    chore: rename cdk

--- cdk/ro-investorcrm-prod-db-to-s3/bin/cdk.ts
+++ cdk/ro-investorcrm-prod-db-to-s3/bin/cdk.ts
@@ -4,8 +4,8 @@ import * as cdk from '@aws-cdk/core';
 import { RdsSnapshotExportPipelineStack, RdsEventId } from '../lib/rds-snapshot-export-pipeline-stack';
 
 const app = new cdk.App();
-new RdsSnapshotExportPipelineStack(app, 'RdsSnapshotExportToS3PipelineRoCrm', {
-  dbName: 'ro-investorcrm-db',
+new RdsSnapshotExportPipelineStack(app, 'RdsSnapshotExportToS3PipelineInvestorCrm', {
+  dbName: 'investorcrm-db',
   rdsEventId: RdsEventId.DB_AUTOMATED_SNAPSHOT_CREATED,
-  s3BucketName: 'ro-investorcrm-db-export',
+  s3BucketName: 'investorcrm-db-export',
 });
