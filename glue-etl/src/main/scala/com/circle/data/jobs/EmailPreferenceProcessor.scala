4178c0ba1301324d7e407e0bc1bfb8226eeeafbc
Jinqiang Han <jinqianghan@ip-192-168-29-169.ec2.internal>
2022-04-22 15:41:13 -0700

    add email preference job

---
+++ glue-etl/src/main/scala/com/circle/data/jobs/EmailPreferenceProcessor.scala
@@ -0,0 +1,59 @@
+package com.circle.data.jobs
+
+import com.amazonaws.services.glue.GlueContext
+import com.amazonaws.services.glue.util.Job
+import com.circle.data.glue.{DataLoader, GlueJob}
+import com.circle.data.utils.Logging
+import com.circle.data.utils.QueryBase._
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode, SparkSession}
+import org.apache.spark.sql.expressions.Window
+
+import java.time.LocalDateTime
+import java.util.Date
+import java.time.format.DateTimeFormatter
+
+/**
+ * Processor for getting SeedInvest user's email preference
+ */
+object EmailPreferenceProcessor
+  extends GlueJob with DataLoader with Logging {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val context: GlueContext = new GlueContext(new SparkContext())
+    implicit val sparkSession: SparkSession = context.sparkSession
+    implicit val sqlContext: SQLContext = sparkSession.sqlContext
+
+    logConfiguration()
+
+    val options = initializeJob(context, args,
+      Array(SourceGlueDatabaseParam, OutputLocationParam)
+    )
+
+    val snapshotDatabase = options(SourceGlueDatabaseParam)
+    val outputFileLocation = options(OutputLocationParam)
+
+    val preferenceRuleData = getDataFrameForGlueCatalog(snapshotDatabase, "public_ponyexpress_preference_rule")
+    val contactChannelData = getDataFrameForGlueCatalog(snapshotDatabase, "public_ponyexpress_preferences_contact_channel")
+    val preferenceData = getDataFrameForGlueCatalog(snapshotDatabase, "public_ponyexpress_preferences_preference")
+
+    val result = getEmailPreferenceData(preferenceRuleData, contactChannelData, preferenceData)
+
+    val timestampKey = LocalDateTime.now.format(DateTimeFormatter.ofPattern("YYYY/MM/dd_HHmmss"))
+    val outputPath = s"$outputFileLocation/$timestampKey"
+
+    result
+      .coalesce(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .option("header", value = true)
+      // Use escape to sets a single character used for escaping quotes inside an already quoted value.
+      .option("escape", "\"")
+      .csv(outputPath)
+
+    Job.commit()
+  }
+
+}
